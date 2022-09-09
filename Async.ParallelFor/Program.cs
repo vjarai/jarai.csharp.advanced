f20d92eb5fb94662aa18fc48134bba00ffe594c8
Viktor <vjarai@users.noreply.github.com>
2022-09-09 11:30:41 +0200

    ParallelFor

---
+++ Async.ParallelFor/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jarai.CSharp.Async.ParallelFor
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 2 million
+            var limit = 2_000_000;
+            var numbers = Enumerable.Range(0, limit).ToList();
+
+            var watch = Stopwatch.StartNew();
+            var primeNumbersFromForeach = GetPrimeList(numbers);
+            watch.Stop();
+
+            var watchForParallel = Stopwatch.StartNew();
+            var primeNumbersFromParallelForeach = GetPrimeListWithParallel(numbers);
+            watchForParallel.Stop();
+
+            var watchForPlinq = Stopwatch.StartNew();
+            var primeNumbersFromPlinq = GetPrimeListWithPlinq(numbers);
+            watchForPlinq.Stop();
+
+            Console.WriteLine($"Classical foreach loop | Total prime numbers : {primeNumbersFromForeach.Count} | Time Taken : {watch.ElapsedMilliseconds} ms.");
+            Console.WriteLine($"Parallel.ForEach loop  | Total prime numbers : {primeNumbersFromParallelForeach.Count} | Time Taken : {watchForParallel.ElapsedMilliseconds} ms.");
+            Console.WriteLine($"Parallel.ForEach loop  | Total prime numbers : {primeNumbersFromPlinq.Count} | Time Taken : {watchForPlinq.ElapsedMilliseconds} ms.");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// GetPrimeList returns Prime numbers by using sequential ForEach
+        /// </summary>
+        /// <returns></returns>
+        private static IList<int> GetPrimeList(IList<int> numbers)
+        {
+            return numbers.Where(IsPrime).ToList();
+        }
+
+        /// <summary>
+        /// GetPrimeList returns Prime numbers by using sequential ForEach
+        /// </summary>
+        /// <returns></returns>
+        private static IList<int> GetPrimeListWithPlinq(IList<int> numbers)
+        {
+            return numbers.AsParallel().Where(IsPrime).ToList();  // PLINQ (Parallel Linq)
+        }
+
+        /// <summary>
+        /// GetPrimeListWithParallel returns Prime numbers by using Parallel.ForEach
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private static IList<int> GetPrimeListWithParallel(IList<int> numbers)
+        {
+            var primeNumbers = new ConcurrentBag<int>();
+
+            void AddPrime(int number)
+            {
+                if (IsPrime(number))
+                {
+                    primeNumbers.Add(number);
+                }
+            }
+
+            Parallel.ForEach(numbers, AddPrime);  // Ausführung auf mehreren Tasks
+
+            return primeNumbers.ToList();
+        }
+
+        /// <summary>
+        /// IsPrime returns true if number is Prime, else false.(https://en.wikipedia.org/wiki/Prime_number)
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (var divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
