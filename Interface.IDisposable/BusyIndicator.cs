92a319286bc77ffb7358fc79d436a68c7a0028b8
Viktor <vjarai@users.noreply.github.com>
2022-10-06 08:31:43 +0200

    Dispose

---
+++ Interface.IDisposable/BusyIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Jarai.CSharp.Interface.Disposable
+{
+    internal class BusyIndicator : IDisposable
+    {
+        public BusyIndicator()
+        {
+            Console.WriteLine("Berechnung gestartet (Sanduhr an).");
+            
+            // Todo: Sanduhr anzeigen...
+        }
+
+        /// <summary>
+        /// Die Dispose Methode wird IMMER automatisch beim verlassen des zugehörigen using Blocks aufgerufen.
+        /// (Auch bei einer Exception!)
+        /// </summary>
+        public void Dispose()
+        {
+            Console.WriteLine("Berechnung beenden (Sanduhr aus).");
+
+            // Todo: Sanduhr ausblenden...
+        }
+    }
+}
