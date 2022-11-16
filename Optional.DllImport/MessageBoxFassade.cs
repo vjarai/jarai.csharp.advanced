f1acf0be34fa26f1d9f83a41e137db34754c6cfa
Viktor <viktor@nospam.org>
2022-11-16 06:57:55 +0100

    .

---
+++ Optional.DllImport/MessageBoxFassade.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Optional.DllImport
+{
+    public sealed class MessageBoxFassade
+    {
+    
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+   
+        public static void ShowMessageBox(string caption, string text)
+        {
+            MessageBox(IntPtr.Zero, caption, text, 0);
+        }
+
+    }
+}
---
+++ Optional.DllImport/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Optional.DllImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBoxFassade.ShowMessageBox("Hallo Welt", "Titel");
+        }
+    }
+}
