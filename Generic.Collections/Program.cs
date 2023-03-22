35b20c8aa64961445a7eca9d98633995a69d8c3a
Viktor <viktor@nospam.org>
2023-03-22 11:27:46 +0100

    .

--- Generic.Collections/Program.cs
+++ Generic.Collections/Program.cs
@@ -1,50 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace Jarai.CSharp.Generic.Collections;
 
-namespace Jarai.CSharp.Generic.Collections
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static List<double> _myList;
-        private static double[] _myArray;
-        private static Queue<int> _myQueue;
-        private static Stack<int> _myStack;
-        private static Dictionary<string, string> _myDictionary;
+        // Listen können zur Laufzeit erweitert werden
+        var myList = new List<double> { 10, 50, 30, 20 };
+        myList.Add(999);
+        foreach (var item in myList) Console.WriteLine(item);
 
-        private static void Main(string[] args)
+        // Queues geben Elemente in gleicher Reihenfolge wieder aus (FIFO)
+        var myQueue = new Queue<int>();
+        myQueue.Enqueue(10);
+        myQueue.Enqueue(50);
+        Console.WriteLine(myQueue.Dequeue()); // 10
+        Console.WriteLine(myQueue.Dequeue()); // 50
+
+        // Stacks geben Elemente in umgekehrter Reihenfolge wieder aus (FILO)
+        var myStack = new Stack<int>();
+        myStack.Push(10);
+        myStack.Push(50);
+        Console.WriteLine(myStack.Pop()); // 50
+        Console.WriteLine(myStack.Pop()); // 10
+
+        // Ein Dictionary ermöglicht den schnellen Zugriff auf ein element über einen key
+        var myDictionary = new Dictionary<string, string>
         {
-            // Listen können zur Laufzeit erweitert werden
-            _myList = new List<double> { 10, 50, 30, 20 };
-            _myList.Add(999);
+            { "Gut", "Good" },
+            { "Schlecht", "Bad" }
+        };
 
-            // Arrays können indiziert werden (random access)
-            _myArray = _myList.ToArray();
-            Console.WriteLine(_myArray[2]);
+        myDictionary.Add("ok", "ok");
 
-            // Queues geben Elemente in gleicher Reihenfolge wieder aus (FIFO)
-            _myQueue = new Queue<int>();
-            _myQueue.Enqueue(10);
-            _myQueue.Enqueue(50);
-            Console.WriteLine(_myQueue.Dequeue());  // 10
-            Console.WriteLine(_myQueue.Dequeue());  // 50
-
-            // Stacks geben Elemente in umgekehrter Reihenfolge wieder aus (FILO)
-            _myStack = new Stack<int>();
-            _myStack.Push(10);
-            _myStack.Push(50);
-            Console.WriteLine(_myStack.Pop()); // 50
-            Console.WriteLine(_myStack.Pop()); // 10
-
-            // Ein Dictionary ermöglicht den schnellen Zugriff auf ein element über einen key
-            _myDictionary = new Dictionary<string, string>
-            {
-                { "Gut", "Good" },
-                { "Schlecht", "Bad" }
-            };
-
-            _myDictionary.Add("ok", "ok");
-
-            Console.WriteLine(_myDictionary["Gut"]); // Good
-        }
+        Console.WriteLine(myDictionary["Gut"]); // Good
     }
 }
